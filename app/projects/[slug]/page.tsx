71fd8e52664a2cbf5ac8851188c05a3374fe9158
Arrownymous <stijnschoonderwoerd@gmail.com>
2026-03-06 12:05:13 +0100

    better pictures

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -14,7 +14,7 @@ function renderContent(content: string) {
     .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
     .replace(/\*(.+?)\*/g, '<em>$1</em>')
     .replace(/`([^`]+)`/g, '<code>$1</code>')
-    .replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1" style="max-width:100%;margin:20px 0;display:block;" />')
+    .replace(/!\[([^\]]*)\]\(([^)]+)\)/g, '<img src="$2" alt="$1" style="width:300px;max-width:100%;margin:20px 0;display:block;" />')
     .replace(/- \[x\] (.+)/g, '<li class="check done">$1</li>')
     .replace(/- \[ \] (.+)/g, '<li class="check todo">$1</li>')
     .replace(/(<li class="check[^>]*>.*<\/li>\n?)+/g, (m) => `<ul class="checklist">${m}</ul>`)
